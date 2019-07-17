10c2bddb633a27d63b0c4d750e2fc4b7c5caf6ba
Jasmine Walters <jsmnwltrs@gmail.com>
2019-07-16 20:34:42 -0500

    dependency injection working. simple api calls are retreiving data

---
+++ MonsterMonitor/Controllers/SightingsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using MonsterMonitor.Data;
+using MonsterMonitor.Models;
+
+namespace MonsterMonitor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SightingsController : ControllerBase
+    {
+        readonly SightingRepository _sightingRepository;
+
+        public SightingsController(SightingRepository sightingRepository)
+        {
+            _sightingRepository = sightingRepository;
+        }
+
+        [HttpGet("all")]
+        public ActionResult GetAllSightings()
+        {
+            var sightings = _sightingRepository.getAll();
+
+            return Ok(sightings);
+        }
+    }
+}
---
+++ MonsterMonitor/Data/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Options;
+using MonsterMonitor.Modals;
+
+namespace MonsterMonitor.Data
+{
+    public class UserRepository
+    {
+        readonly string _connectionString;
+
+        public UserRepository(IOptions<DbConfiguration> dbConfig)
+        {
+            _connectionString = dbConfig.Value.ConnectionString;
+        }
+
+        public List<User> GetAll()
+        {
+            using(var db = new SqlConnection(_connectionString))
+            {
+                var userList = db.Query<User>(
+                    @"select *
+                    from Users"
+                ).ToList();
+
+                return userList;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
