98e4ba6d84f0c10c35616e6840aaab2ac5b9b765
Jasmine Walters <jsmnwltrs@gmail.com>
2019-07-20 09:57:14 -0500

    finished basic controllers

--- MonsterMonitor/Data/UserRepository.cs
+++ MonsterMonitor/Data/UserRepository.cs
@@ -32,5 +32,66 @@ namespace MonsterMonitor.Data
 
             throw new NotImplementedException();
         }
+
+        internal object GetById(int userId)
+        {
+            using(var db = new SqlConnection(_connectionString))
+            {
+                var user = db.QueryFirstOrDefault<User>(
+                    @"select *
+                    from Users
+                    where Id = @userId",
+                    new { userId });
+
+                if (user != null)
+                {
+                    return user;
+                }
+            }
+
+            throw new Exception("User not Found");
+        }
+
+        internal object Add(string username, string email, string imageUrl, string location)
+        {
+            using(var db = new SqlConnection(_connectionString))
+            {
+                var newUser = db.QueryFirstOrDefault<User>(
+                    @"insert into Users(Username, Email, ImageUrl, Location)
+                    output inserted.*
+                    values(@username, @email, @imageUrl, @location)",
+                    new { username, email, imageUrl, location});
+
+                if (newUser != null)
+                {
+                    return newUser;
+                }
+            }
+
+            throw new Exception("User did not add");
+        }
+
+        internal object Update(int id, string username, string email, string imageUrl, string location)
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var updatedUser = db.QueryFirstOrDefault<User>(
+                    @"update Users
+                    set Username = @username,
+                    Email = @email,
+                    ImageUrl = @imageUrl,
+                    Location = @location
+                    output inserted.*
+                    where Id = @id",
+                    new { username, email, imageUrl, location, id });
+
+                if (updatedUser != null)
+                {
+                    return updatedUser;
+                }
+            }
+
+            throw new Exception("User did not update");
+        }
     }
 }
